d2aadaa32bc50b72b0a4cd44070aab4b673908ea
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2023-03-26 22:05:44 -0300

    complete closure - ex5

--- closures/exercises/ex5.js
+++ closures/exercises/ex5.js
@@ -2,11 +2,23 @@
 
 // Write a function after that takes the number of times the callback needs to be called before being executed as the first parameter and the callback as the second parameter.
 
-function after(count, func) {}
+function after(count, func) {
+  let counter = 0;
+  function executeFunctionAfterTimes() {
+    counter++;
+    if (counter == count) {
+      func();
+    }
+  }
+
+  return executeFunctionAfterTimes;
+}
 
 // /*** Uncomment these to check your work! ***/
-// const called = function() { console.log('hello') };
-// const afterCalled = after(3, called);
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => 'hello' is printed
+const called = function () {
+  console.log("hello");
+};
+const afterCalled = after(3, called);
+afterCalled(); // => nothing is printed
+afterCalled(); // => nothing is printed
+afterCalled(); // => 'hello' is printed
